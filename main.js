c5edd9f7033d0166118be79720c9f0479fbaf181
ashutosh08082003 <76652874+ashutosh08082003@users.noreply.github.com>
2021-08-26 00:56:29 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+function changeImage1() {
+    document.getElementById("pro-img").src = "image/2image.jpg";
+  }
+
+function changeImage2() {
+    document.getElementById("pro-img").src = "image/image.png";
+  }
+
+function changeImage3() {
+    document.getElementById("pro-img").src = "image/3image.jpg";
+ }
+
+
+// jQuery counterUp
+$('[data-toggle="counter-up"]').counterUp({
+  delay: 10,
+  time: 1000
+});
